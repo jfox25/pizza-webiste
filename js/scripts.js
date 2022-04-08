a49914f6bbb3589f6100c7420e2d2a89566490f1
James Fox <jimmyfox00@gmail.com>
2022-04-08 09:05:28 -0700

    First Order test passes

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,14 @@ function Order() {
   this.pizzas = {};
   this.currentId = 0;
 }
+
+Order.prototype.addPizza = function (pizza) {
+  // pizza.id = this.assignId();
+  this.pizzas[pizza.size] = pizza;
+};
+
+function Pizza(toppings, size) {
+  console.log(toppings);
+  this.toppings = toppings;
+  this.size = size;
+}
